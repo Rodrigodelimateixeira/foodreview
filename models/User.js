bcb78d7520d2e4945a360a0ffb08cd86a230bc7f
rodrigodelimateixeira <delimateixeirarodrigo@gmail.com>
2022-08-16 22:08:52 -0300

    aula 16/08/2022

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import Sequelize from "sequelize";
 import connection from "../config/db.js"
+import bcrypt from "bcrypt";
+
 
 const User = connection.define(
     'user',
@@ -33,6 +35,23 @@ const User = connection.define(
             type: Sequelize.BOOLEAN,
             allowNull: false,
             
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async(user) => {
+                if(user.password){
+                    const salt = await bcrypt.genSaltSync(10, 'a');
+                    user.password = bcrypt.hashSync(user.password, salt);
+                }
+            },
+
+            beforeUpdate: async(user) => {
+                if(user.password){
+                    const salt = await bcrypt.genSaltSync(10, 'a');
+                    user.password = bcrypt.hashSync(user.password, salt);
+                }
+            }
         }
     }
 );

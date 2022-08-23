dd21cb62d0bf981abfcf9881acf24e638a749fd5
rodrigodelimateixeira <delimateixeirarodrigo@gmail.com>
2022-08-22 21:48:11 -0300

    aula 22/08/2022

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,10 +1,52 @@
 import express from 'express';
-
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import Jwt  from 'jsonwebtoken';
 
 const login = express.Router();
 
 
-login.get('/', (req,res)=> res.send("Rota de Login"));
+login.post('/', async (req,res)=> {
+    const {email, password} = req.body;
+    
+    // 1º - pesquisar a existência deste usuário no BD
+
+    const registeredUser = await User.findOne({ where: { email } }).catch(
+        (err) => {
+            console.log("error: ", err);
+        }
+    );
+
+    // 2º - Caso inexistente, informar usuário
+        
+        if(!registeredUser)
+          return res
+            .status(400)
+            .json({ message:  "Email ou Senha incorretos. "});
+
+
+    // 3º - Validar a senha
+    // 3.1 - Caso senha inválida, informar usuário
+
+        if( !bcrypt.compareSync(password, registeredUser.password) )
+         return res
+            .status(400)
+            .json({ message:  "Email ou Senha incorretos. "});
+
+
+    // 3.2 - Caso senha correta, gerar token
+
+        const token = Jwt.sign(
+            { id: registeredUser.id, email: registeredUser.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+    // 3.3 - Enviar token ao usuário
+         
+         res.json( {message: "Bem-Vindo!", token: token} );   
+
+});
 
 
 export default login;
